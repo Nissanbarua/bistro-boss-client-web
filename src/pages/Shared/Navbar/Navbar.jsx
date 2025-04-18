882dd50a11c8d57d288e11537921a4d143fb997b
Nissan Barua <baruanissan55@gmail.com>
2025-04-18 23:36:29 +0600

    assets file updated and banner section complete

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
   );
   return (
     <>
-      <div className="navbar bg-base-100 shadow-sm">
+      <div className="container mx-auto z-10 navbar fixed text-black ">
         <div className="navbar-start">
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
