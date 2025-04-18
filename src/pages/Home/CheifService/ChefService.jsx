59023c6adff68f34945e5a29f48e8d03af3e3b51
Nissan Barua <baruanissan55@gmail.com>
2025-04-19 00:53:35 +0600

    chef service section added

---
+++ src/pages/Home/CheifService/ChefService.jsx
@@ -0,0 +1,18 @@
+import "./CheifService.css";
+const ChefService = () => {
+  return (
+    <div className="chefService-section flex justify-center items-center mb-10">
+      <div className="bg-white text-center p-4  w-2/3 mx-auto">
+        <h2 className="text-4xl m-4">Bistro Boss</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Necessitatibus, libero accusamus laborum deserunt ratione dolor
+          officiis praesentium! Deserunt magni aperiam dolor eius dolore at,
+          nihil iusto ducimus incidunt quibusdam nemo.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default ChefService;
